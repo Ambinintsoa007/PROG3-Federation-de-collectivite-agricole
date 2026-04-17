6b6ce86151537667f29d056840f8e36857b85cdb
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-04-17 14:12:35 +0300

    feat: project structure

---
+++ src/test/java/Federation/Agricole/API/FederationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Federation.Agricole.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
