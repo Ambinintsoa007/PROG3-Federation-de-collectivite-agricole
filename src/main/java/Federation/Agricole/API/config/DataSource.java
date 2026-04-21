fe1a8c55fa6e718fb01d7df6420603a80b8d5841
Natoltora123 <nhasimbolatoavina@gmail.com>
2026-04-21 16:57:58 +0300

    feat: add datasource

---
+++ src/main/java/Federation/Agricole/API/config/DataSource.java
@@ -0,0 +1,32 @@
+package org.demo.td5.config;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+public class DataSource {
+    private final String jdbcUrl = System.getenv("JDBC_URL");
+    private final String user = System.getenv("USERNAME");
+    private final String password = System.getenv("PASSWORD");
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(jdbcUrl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
