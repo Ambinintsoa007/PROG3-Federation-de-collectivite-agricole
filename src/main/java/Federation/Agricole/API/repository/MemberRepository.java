63aa132df1a0293d3632055a2ad8db38d907601e
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-04-21 22:24:07 +0300

    feat: update MemberRepository, add save method

--- src/main/java/Federation/Agricole/API/repository/MemberRepository.java
+++ src/main/java/Federation/Agricole/API/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package Federation.Agricole.API.repository;
 
 import Federation.Agricole.API.config.DataSource;
+import Federation.Agricole.API.entity.Member;
+import Federation.Agricole.API.entity.MemberOccupation;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
@@ -30,5 +32,35 @@ public class MemberRepository {
             throw new RuntimeException(e);
         }
         return false;
+    }
+
+    public void save(Member member) {
+        String sql = """
+                INSERT INTO members 
+                (id, first_name, last_name, birth_date, gender, address, 
+                profession, phone_number, email, occupation, is_founder)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ){
+          pstmt.setString(1, member.getId());
+          pstmt.setString(2, member.getFirstName());
+          pstmt.setString(3, member.getLastName());
+          pstmt.setObject(4, member.getBirthday());
+          pstmt.setString(5, member.getGender().name());
+          pstmt.setString(6, member.getAddress());
+          pstmt.setString(7, member.getProfession());
+          pstmt.setInt(8, member.getPhoneNumber());
+          pstmt.setString(9, member.getEmail());
+          pstmt.setString(10, member.getOccupation().name());
+          pstmt.setBoolean(11, member.isFounder());
+
+          pstmt.executeUpdate();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
